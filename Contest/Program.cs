f0adc2e76da0d166a17994c8b74e62436c7b37fe
Huu Tien Nguyen <huutien.nguyen@ucg.co.nz>
2023-06-12 15:41:22 +0700

    B4 update

--- Contest/Program.cs
+++ Contest/Program.cs
@@ -8,26 +8,59 @@ namespace contest
     {
         static void Main(string[] args)
         {
-            Console.Write("Input number: ");
-            int n = int.Parse(Console.ReadLine());
-            long standingCount = (long)Math.Sqrt(n);
-            Console.WriteLine("Count Sqrt: " + standingCount);
-
-            Console.WriteLine("Count while: " + CountStand(n));
-        }
-        static int CountStand(long n)
-        {
-            if (n < 1) return 0;
-            if (n < 4) return 1;
-            int num = 1;
-            int start = 1;
-            while (n > start)
+            int T = int.Parse(Console.ReadLine());
+            List<string> L1 = new List<string>();
+            List<string> L2 = new List<string>();
+            for (int i = 0; i < T; i++)
             {
-                start = start + 2;
-                n = n - start;
-                if (n > 0) num++;
+                L1.Add(Console.ReadLine());
+                L2.Add(Console.ReadLine());
             }
-            return num;
+            for (int i = 0; i < T; i++)
+            {
+                char[] s1 = L1[i].ToCharArray();
+                char[] s2 = L2[i].ToCharArray();                
+                Console.WriteLine(B4_Process(s1, s2));
+            }
+        }
+
+        static string B4_Process(char[] s1, char[] s2)
+        {
+            int len1 = s1.Length;
+            int len2 = s2.Length;
+            char[] result = new char[len1 + len2];
+            int i = 0;
+            int j = 0;
+            for (int s = 0; s < result.Length; s++)
+            {
+                if (i < len1 && j < len2)
+                {
+                    if (s1[i] < s2[j])
+                    {
+                        result[s] = s1[i];
+                        i++;
+                    }
+                    else
+                    {
+                        result[s] = s2[j];
+                        j++;
+                    }
+                }
+                else
+                {
+                    if (i < len1)
+                    {
+                        result[s] = s1[i];
+                        i++;
+                    }
+                    else
+                    {
+                        result[s] = s2[j];
+                        j++;
+                    }
+                }
+            }
+            return new string(result);
         }
     }
 }
