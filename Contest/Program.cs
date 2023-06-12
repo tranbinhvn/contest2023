ac692633bd06c2e243a3263ea4eb9387c5e1ed37
Huu Tien Nguyen <huutien.nguyen@ucg.co.nz>
2023-06-12 08:15:42 +0700

    B2

--- Contest/Program.cs
+++ Contest/Program.cs
@@ -8,7 +8,26 @@ namespace contest
     {
         static void Main(string[] args)
         {
-            
-        }       
+            Console.Write("Input number: ");
+            int n = int.Parse(Console.ReadLine());
+            long standingCount = (long)Math.Sqrt(n);
+            Console.WriteLine("Count Sqrt: " + standingCount);
+
+            Console.WriteLine("Count while: " + CountStand(n));
+        }
+        static int CountStand(long n)
+        {
+            if (n < 1) return 0;
+            if (n < 4) return 1;
+            int num = 1;
+            int start = 1;
+            while (n > start)
+            {
+                start = start + 2;
+                n = n - start;
+                if (n > 0) num++;
+            }
+            return num;
+        }
     }
 }
